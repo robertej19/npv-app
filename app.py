ebebc5e16e20a0a4aa1224e4ae4b420ff8c67825
robertej19 <robertej@mit.edu>
2020-06-11 16:18:57 -0400

    server

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div([
     #html.H1('PPV and NPV'),
 
